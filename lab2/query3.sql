9b61accee936061b44fba03917336d478c652f4a
Jake Armendariz <jakearmendariz99@gmail.com>
2021-02-05 19:27:43 -0800

    bugs

--- lab2/query3.sql
+++ lab2/query3.sql
@@ -4,7 +4,7 @@
 
 SELECT p1.productID, p1.manufacturer, p1.regularPrice
 FROM Products p1
-WHERE (SELECT MAX(p2.regularPrice)
+WHERE p1.regularPrice >= (SELECT MAX(p2.regularPrice)
        FROM Products p2
        WHERE p2.manufacturer = p1.manufacturer
          AND p1.productID <> p1.productID);
