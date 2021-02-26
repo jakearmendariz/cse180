bee6febc1635da71ff8039eb717f59437ea5b97b
Jake Armendariz <jakearmendariz99@gmail.com>
2021-02-25 23:00:00 -0800

    general

--- lab3/general.sql
+++ lab3/general.sql
@@ -12,11 +12,13 @@ CHECK (regularPrice > 0);
 -- 2. In ShoppingTrips, the value of payType must be either ‘N’, ‘V’, ‘A’, ‘C’ or NULL.
 
 ALTER TABLE ShoppingTrips
-CHECK (payType = 'N' OR payType = 'V' OR payType = 'A' OR payType = 'C' OR payType = NULL);
+ADD CONSTRAINT paytype_valid
+CHECK (payType LIKE 'N' OR payType LIKE 'V' OR payType LIKE 'A' OR payType LIKE 'C' OR payType IS NULL);
 
 
 -- 3. In Shoppers, if shopperName is NULL, then joinDate must also be NULL.
 ALTER TABLE Shoppers
+ADD CONSTRAINT shopper_join_null_rule
 CHECK ((shopperName IS NOT NULL) OR (shopperName IS NULL AND joinDate IS NULL));
 -- Write commands to add general constraints in the order the constraints are described above, and save your
 -- commands to the file general.sql. (Note that UNKNOWN for a Check constraint is okay, but FALSE isn’t.)
