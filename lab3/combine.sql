973f26fc34e4cb95fdd4301be8ecb0aa98cf8a14
Jake Armendariz <jakearmendariz99@gmail.com>
2021-02-28 22:56:47 -0800

    yoloswag6969xxx

--- lab3/combine.sql
+++ lab3/combine.sql
@@ -1,22 +1,3 @@
--- Write a file, combine.sql (which should have multiple SQL statements that are in a Serializable transaction)
--- that will do the following. For each tuple in PurchaseChanges, there might already be a tuple in the Purchases
--- that has the same primary key (that is, the same value for shopperID, tripTimestamp and productID). If there
--- isn’t a tuple in Purchases with the same primary key, then this is a new purchase that should be inserted into
--- Purchases. If there already is a tuple in Purchases with that primary key, then this is an update of information
--- about that purchase. So here are the actions that you should take.
-
---  -- • If there isn’t already a tuple in the Purchases table which has that shopperID, tripTimestamp and
-    -- productID, then insert a tuple into the Purchases table corresponding to that PurchaseChanges tuple. Use
-    -- paidPrice as provided in the PurchaseChanges tuple, and set quantity to 1
-
---  -- • If there already is a tuple in the Purchases table which has that shopperID, tripTimestamp and productID,
-    -- then update the tuple in Purchases that has that shopperID, tripTimestamp and productID. Update
-    -- paidPrice for that existing Purchases tuple based on the value of paidPrice in the PurchaseChanges tuple,
-    -- and increase quantity by 2.
-
--- Your transaction may have multiple statements in it. The SQL constructs that we’ve already discussed in class
--- are sufficient for you to do this part (which is one of the hardest parts of Lab3).
-
 BEGIN TRANSACTION ISOLATION LEVEL SERIALIZABLE;
 
 INSERT INTO Purchases(shopperID, tripTimestamp, productID, quantity, paidPrice)
--- lab3/foreign.sql
+++ lab3/foreign.sql
@@ -1,45 +1,18 @@
--- Important: Before running Sections 2.3, 2.4 and 2.5, recreate the Lab3 schema using the lab3_create.sql
--- script, and load the data using the script lab3_data_loading.sql. That way, any database changes that you’ve
--- done for Combine won’t propagate to these other parts of Lab3.
+-- foreign.sql Jake Armendariz
 
--- Here’s a description of the Foreign Keys that you need to add for this assignment. (Foreign Key Constraints are
--- also referred to as Referential Integrity constraints. The lab3_create.sql file that we’ve provided for Lab3
--- includes the same Referential Integrity constraints that were in the Lab2 solution, but you’re asked to use
--- ALTER to add additional constraints to the Supermarket schema.
-
--- The load data that you’re provided with should not cause any errors when you add these constraint. Just add the
--- constraints listed below, exactly as described, even if you think that additional Referential Integrity constraints
--- should exist. Note that (for example) when we say that every manager (managerID) in the Markets table must
--- appear in the Employees table, that means that the managerID attribute of the Markets table is a Foreign Key
--- referring to the Primary Key of the Employees table.
-
-    -- Each manager (managerID) in the Markets table must appear in the Employees table as an empID.
-    -- (Explanation of what that means appear in the above paragraph.) If an Employees tuple is deleted and
-    -- there is a Markets that refers to that employee, then the deletion should be rejected. Also, if the
-    -- Primary Key of an Employees tuple is updated, and there is a market whose manager corresponds to
-    -- that person, then the update should also be rejected.
-
+--  #1
 ALTER TABLE Markets
 ADD FOREIGN KEY (managerID) REFERENCES Employees (empID)
 ON DELETE RESTRICT
 ON UPDATE RESTRICT;
 
-    -- Each market (marketID) that’s in the Employees table must appear in the Markets table as a marketID.
-    -- If a tuple in the Markets table is deleted, then all Employees in which that market (marketID) appears
-    -- should also be deleted. If the Primary Key (marketID) of a Markets tuple is updated, then all
-    -- Employees tuples who have that marketID should also be updated, getting the same new value for their
-    -- marketID.
+-- #2
 ALTER TABLE Employees
 ADD FOREIGN KEY (marketID) REFERENCES Markets (marketID)
 ON DELETE CASCADE
 ON UPDATE CASCADE ;
 
-    -- Each market (marketID) that’s in the ShoppingTrips table must either be NULL or appear in the
-    -- Markets table as a marketID. If a tuple in the Markets table is deleted, then the marketID for all tuples
-    -- in the ShoppingTrips table which match that marketID should be set to NULL. If the Primary Key
-    -- (marketID) of a Markets tuple is updated, then all ShoppingTrips tuples that have the corresponding
-    -- marketID should also be updated, getting the same new value for their marketID.
-
+-- #3
 ALTER TABLE ShoppingTrips
 ADD FOREIGN KEY (marketID) REFERENCES Markets (marketID)
 ON UPDATE CASCADE
