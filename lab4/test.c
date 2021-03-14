fd52aef0d724deaccd434d07914df0a68814fdba
Jake Armendariz <jakearmendariz99@gmail.com>
2021-03-13 18:17:16 -0800

    rm test

--- lab4/test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-int main() {
-char *query = (char*)malloc(255 * sizeof(char));
-sprintf(query, "%s got back", NULL);
-printf("%s\n", query);
-}
